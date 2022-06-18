1bc57d79a6312b17908a66588a184c46b17b3818
AinulBotz <107542885+AinulBotz@users.noreply.github.com>
2022-06-18 21:02:59 +0800

    Update help.js

--- language/help.js
+++ language/help.js
@@ -418,15 +418,8 @@ return`  _Selamat ${salam} kak ${pushname}_
   *≻* ${prefix}setallmenu
   
   *THANKS TO*
-  *≻* DikaArdnt
-  *≻* ZeeoneOfc
-  *≻* FatihArridho
-  *≻* Yoga
-  *≻* Riy
-  *≻* Deff
-  *≻* Sanzy
-  *≻* Nekel
-  *≻* Bagas Prdn
+  *≻* AinulBotz
+  *≻* RifBot
   `
 }
 
@@ -456,19 +449,13 @@ Thanks!
 —————「 *SEWA | RUN BOT* 」—————
 
   *_SEWA BOT_* 
-  *≻* Sewa Bot 15k (1 minggu)
+  *≻* Sewa Bot 5k (1 minggu)
   *≻* Sewa Bot 20k (1 bulan)
-  *≻* Sewa Bot 25k (Permanen)
 
   *JADI BOT* 
-  *≻* Jadi Bot 15k Tidak Owner (1 minggu)
-  *≻* Jadi Bot + Owner 20k (1 bulan)
-  *≻* Jadi Bot + Owner 25k (Permanen)
+  *≻* Jadi Bot 5k Tidak Owner (1 minggu)
+  *≻* Jadi Bot + Owner 20k (1 bulan) 
 
-  *JASA RUN BOT*
-  *≻* Run Bot 15k (1 minggu)
-  *≻* Run Bot 20k (1 bulan)
-  *≻* Run Bot 25k (Permanen)
 
   *Minat? Langsung chat aja ya cuy 🐦*
 
@@ -953,14 +940,7 @@ exports.ownermenu = (prefix) => {
 exports.thanksto = () => {
 	return`
   *THANKS TO*
-  *≻* DikaArdnt
-  *≻* ZeeoneOfc
-  *≻* FatihArridho
-  *≻* Yoga
-  *≻* Riy
-  *≻* Deff
-  *≻* Sanzy
-  *≻* Nekel
-  *≻* Bagas Prdn
+  *≻* AinulBotz
+  *≻*RifBot
 `
 }
